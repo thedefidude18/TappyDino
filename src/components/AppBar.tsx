5819510f574757fef2a02b5391e794db49aa2284
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-11-11 19:00:00 +0100

    Update AppBar.tsx

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -2,61 +2,57 @@ import { Link, useLocation } from "react-router-dom";
 import { cn } from "../lib/utils";
 
 const links = [
-  { name: "Tappy", link: "/", image: "/images/explore.png" },
-  { name: "Missions", link: "/missions", image: "/images/missions.png" },
+  { name: "Home", link: "/", image: "/images/explore.png" },
   { name: "Friends", link: "/friends", image: "/images/friends.png" },
+  { name: "Tappy", link: "/game", image: "/images/main_button.png" },
   { name: "Tasks", link: "/earn", image: "/images/bounty.png" },
-  { name: "Wallet", link: "/wallet", image: "/images/toncoin.png" },
+  { name: "Rewards", link: "/airdrop", image: "/images/toncoin.png" },
 ];
 
 export default function AppBar() {
   const { pathname } = useLocation();
+
   return (
     <>
-      {/* Import font directly in the component */}
       <style>
         {`
           @import url('https://fonts.googleapis.com/css2?family=ZCOOL+KuaiLe&display=swap');
-          
           .zcool-font {
             font-family: 'ZCOOL KuaiLe', cursive;
           }
         `}
       </style>
 
-      <div className="fixed left-0 z-10 w-full px-5 py-0 bottom-2">
-        <div className="flex items-center w-full p-2 max-w-lg mx-auto rounded-[35px] bg-black/50 backdrop-blur-1xl">
-          {links.map((link, key) => (
-            <Link
-              key={key}
-              to={link.link}
+      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl flex justify-around items-center z-40 text-xs">
+        {links.map((link, key) => (
+          <Link
+            key={key}
+            to={link.link}
+            className="flex-1 bg-black py-0 px-[1px]"
+          >
+            <div
               className={cn(
-                "relative flex items-center rounded-xl flex-col justify-center font-bold text-xs px-2.5 py-1.5 gap-1 select-none flex-1 text-white/30",
-                pathname === link.link && " text-white",
-                "zcool-font" // Apply the font class here
+                "flex flex-col items-center justify-center h-16 p-2 transition-all duration-300",
+                pathname === link.link
+                  ? "bg-[linear-gradient(180deg,_#000000_0%,_#1461C9_24.5%,_#1461C9_49%,_#1461C9_100%)] h-full w-full mt-[-5px]"
+                  : "text-[#85827d] bg-[#17307B]"
               )}
             >
-              {link.image && (
-                <img
-                  src={link.image}
-                  alt={link.name}
-                  className={cn(
-                    "w-7 h-7 object-contain filter grayscale",
-                    pathname === link.link && "filter-none"
-                  )}
-                />
-              )}
-              <span>{link.name}</span>
-              <div
-                className={cn(
-                  "absolute hidden -bottom-1 left-1/2 -translate-x-1/2 bg-[#D9D9D9] rounded-sm shadow-[0px_0px_4px_0px_#B88CFF] h-1 w-4/5",
-                  pathname === link.link && "block"
+              <div className="w-8 h-8 relative">
+                {link.image && (
+                  <img
+                    src={link.image}
+                    alt={link.name}
+                    className="w-full h-full object-contain"
+                  />
                 )}
-              />
-            </Link>
-          ))}
-        </div>
+              </div>
+              <p className="mt-1 zcool-font text-white">{link.name}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </>
   );
 }
+
