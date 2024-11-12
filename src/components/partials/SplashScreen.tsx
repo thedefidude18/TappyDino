9951ec8023ec343aa8bdc9950461a6231aecc6d5
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-11-12 14:26:22 +0100

    Update SplashScreen.tsx

--- src/components/partials/SplashScreen.tsx
+++ src/components/partials/SplashScreen.tsx
@@ -5,17 +5,17 @@ import YoutubeIcon from "../icons/YoutubeIcon";
 const socialLinks = [
   {
     title: "Telegram",
-    url: "#",
+    url: "",
     icon: TelegramIcon,
   },
   {
     title: "Youtube",
-    url: "#",
+    url: "",
     icon: YoutubeIcon,
   },
   {
     title: "Twitter",
-    url: "#",
+    url: "",
     icon: TwitterIcon,
   },
 ];
@@ -36,7 +36,7 @@ export default function SplashScreen() {
       <div className="flex flex-col items-center w-full">
         <div className="flex flex-col items-center w-full pb-6 bg-[url('/images/blur.png')] bg-cover bg-center">
           <h1 className="text-6xl font-medium text-center uppercase text-shadow">
-            Crypto Coin
+            
           </h1>
           <img
             src="/images/loader.png"
@@ -44,9 +44,9 @@ export default function SplashScreen() {
             className="mt-3 animate-spin"
           />
           <p className="mt-3 text-sm font-bold uppercase text-primary">
-            Stay tuned
+            
           </p>
-          <p className="mt-1 font-medium">More info in official channels</p>
+          <p className="mt-1 font-medium"></p>
           <div className="flex items-center gap-4 mt-6">
             {socialLinks.map((link) => (
               <a
