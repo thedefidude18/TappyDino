4074cfa857b505e37afd58c4d84d2476bbbc9b29
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-12-02 10:10:47 +0100

    Update UserTap.tsx

--- src/components/UserTap.tsx
+++ src/components/UserTap.tsx
@@ -92,7 +92,7 @@ export default function UserTap(props: React.HTMLProps<HTMLDivElement>) {
           <img
             src={levelConfig.frogs[user.level?.level || 1]}
             alt="level image"
-            className="object-fill w-35 h-50"
+            className="object-fill w-30 h-50"
             style={{ filter: levelConfig.filter[user.level?.level || 1] }}
           />
         </button>
