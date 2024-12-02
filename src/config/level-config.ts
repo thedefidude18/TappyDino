f3202815fd52e09113a9446cf832fe976aa0f858
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-12-02 08:59:33 +0100

    Update level-config.ts

--- src/config/level-config.ts
+++ src/config/level-config.ts
@@ -2,9 +2,9 @@ const levelConfig = {
   frogs: {
     1: "/images/levels/coin1.gif",
     2: "/images/levels/coin2.gif",
-    3: "/images/levels/coin.png",
-    4: "/images/levels/coin.png",
-    5: "/images/levels/coin.png",
+    3: "/images/levels/coin3.gif",
+    4: "/images/levels/coin4.gif",
+    5: "/images/levels/coin5.gif",
   } as Record<number, string>,
 
   filter: {
